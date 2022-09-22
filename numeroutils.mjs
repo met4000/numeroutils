94f156693bcc1e11a5d7993250a0a7f12fe763d0
met4000 <nathsharwor@hotmail.com>
2022-09-23 03:18:31 +0800

    add toString and modify return type

--- numeroutils.mjs
+++ numeroutils.mjs
@@ -9,67 +9,108 @@ export const CardType = {
 
 export class Card {
   /**
+   * @param {CardType} type
    * @param {(n: number) => number} modFunc the value modification function
    * @param {(n: number) => number} invFunc the inverse of the value modification function
-   * @param {CardType} type
    */
-  constructor(modFunc, invFunc, type) {
+  constructor(type, modFunc, invFunc) {
+    this.type = type;
     this.modFunc = modFunc;
     this.invFunc = invFunc;
-    this.type = type;
   }
-};
+
+  // should be overwritten by subclasses
+  getDescription() { return this; }
+
+  toString() {
+    return `[${this.getDescription()}]`;
+  }
+}
 
 export class NumberCard extends Card {
   /**
    * @param {number} value
    */
   constructor(value) {
-    super(n => n + this.value, n => n - this.value, CardType.NUMBER);
+    super(CardType.NUMBER, n => n + this.value, n => n - this.value);
     this.value = value;
+  }
+
+  getDescription() { return this.value; }
+}
+
+export class WildCard extends Card {
+  /**
+   * @param {(n: number) => number} modFunc 
+   * @param {(n: number) => number} invFunc 
+   */
+  constructor(description, modFunc, invFunc) {
+    super(CardType.WILD, modFunc, invFunc);
+    this.description = description;
+  }
+
+  getDescription() { return this.description; }
+}
+
+export class FractionWildCard extends WildCard {
+  constructor(p, q) {
+    super(`* ${p}/${q}`, n => n * p / q, n => n * q / p);
+    this.p = p;
+    this.q = q;
   }
 }
 
 /**
  * @param {Array<Card>} cardsInPlay 
  * @param {Array<Card>} cardsInHand 
- * @returns {Array<Card>} the longest 'take'
+ * @returns {{ card: Card, take: Array<Card> }} the longest 'take'
  */
 export function findLongestTake(cardsInPlay, cardsInHand) {
   let availableCards = [...cardsInPlay, ...cardsInHand.filter(card => card.type !== CardType.NUMBER)];
   let handNums = cardsInHand.filter(card => card.type === CardType.NUMBER);
 
-  let longestChains = handNums.map((card, i) => longestChainGivenTarget(card.modFunc(0), availableCards, handNums.filter((_, _i) => _i !== i)));
-  let longestChain = longestChains.reduce((runningChain, chain) => runningChain.length > chain.length ? runningChain : chain, []);
-  return reverseArr(longestChain);
+  let longestTakes = handNums.map((card, i) => ({
+    card,
+    take: reverseArr(longestTakeGivenTarget(card.modFunc(0), availableCards, handNums.filter((_, _i) => _i !== i)) ?? [])
+  }));
+  let longestTake = longestTakes.reduce((r, take) => r.take.length > take.take.length ? r : take);
+  return longestTake;
 }
 
 /**
  * @param {number} target 
  * @param {Array<Card>} cards 
  * @param {Array<Card>} handNums TODO: currently unused
- * @returns {Array<Card> | undefined} the longest chain, starting with the target (not included), or undefined if none exist
+ * @returns {Array<Card> | undefined} the longest take starting with the target (not included), or undefined if none exist
  */
-function longestChainGivenTarget(target, cards, handNums) {
-  if (cards.length === 0) return [];
+function longestTakeGivenTarget(target, cards, handNums) {
+  if (cards.length === 0) return undefined; // no more cards
 
-  let longestChain = undefined;
+  let longestTake = undefined;
 
+  // case for playing no cards:
+  if (target === 0) longestTake = [];
+
+  // case for playing cards:
   let leftCards = [], rightCards = [...cards];
   for (let i in cards) {
     let card = rightCards.shift();
     let n = card.invFunc(target);
 
-    // * Possible rules; the website doesn't actually specify any of them *
-    // if (!Number.isInteger(n)) continue;   // number must always be an integer
-    // if (n < 0) continue;                  // number must always be positive
+    (() => {
+      // * Possible rules; the website doesn't actually specify any of them *
+      // if (!Number.isInteger(n)) return;   // number must always be an integer
+      // if (n < 0) return;                  // number must always be positive
 
-    let chain = [card].concat(longestChainGivenTarget(n, leftCards.concat(rightCards), handNums));
+      let lTake = longestTakeGivenTarget(n, leftCards.concat(rightCards), handNums);
+      if (lTake === undefined) return;
+      
+      let take = [card].concat(lTake);
+      if (take.length > (longestTake ?? []).length) longestTake = take;
+    })();
+
     leftCards.push(card);
-    if (chain === undefined) continue;
-
-    if (chain.length > (longestChain ?? []).length) longestChain = chain;
   }
 
-  return longestChain;
+  return longestTake;
 }
